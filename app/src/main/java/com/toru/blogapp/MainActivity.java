b654fc9e37bf43dc43e35296432343c37ec0252d
ruhullahil <ruhullahilt@gmail.com>
2020-04-01 02:12:43 +0600

    init

---
+++ app/src/main/java/com/toru/blogapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.toru.blogapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        openFragment(HomeFragment.newInstance("", ""));
+                        return true;
+                    case R.id.navigation_add:
+                        openFragment(AddFragment.newInstance("", ""));
+                        return true;
+                    case R.id.navigation_bookmark:
+                        openFragment(BookmarkFragment.newInstance("", ""));
+                        return true;
+                    case R.id.navigation_profile:
+                        openFragment(ProfileFragment.newInstance("", ""));
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
